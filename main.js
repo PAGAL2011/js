5808d334191694f28bfd0b40b797bf816358548c
PAGAL2011 <fayziyevanigora89@gmail.com>
2025-05-03 12:32:05 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+let ism = prompt('iltimos ismingizni kiriting')
+console.log(ism);
+
+let fam = prompt(' iltimos familyangizni kiriting')
+console.log(fam);
+
+let yosh = prompt('iltimos yoshingizni kiriting')
+console.log(yosh);
+
+let turar = prompt('qaysi tumanda turasiz ')
+console.log(turar);
+
+let fan = prompt(' qaysi fanni yaxshi korasz')
+console.log(fan);
+
+let yil = prompt('nechinchi yilsiz')
+console.log(yil)
